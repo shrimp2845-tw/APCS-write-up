60d6046038dd3b3538dadc924ebf04659fa09989
shrimp2845-tw <goldencheng15@gmail.com>
2025-06-09 13:17:37 +0800

    Update b964.py

--- p1/b964.py
+++ p1/b964.py
@@ -1,1 +1,23 @@
-
+#2025-05-01
+n=int(input())
+l=list(map(int,input().split()))
+bc=100
+wc=-100
+for i in l:
+    p=i-60
+    if i>=60:
+        if p<bc:
+            bc=p
+    else:
+        if p>wc:
+            wc=p
+l.sort()
+print(' '.join(list(map(str,l))))
+if wc>=-60:
+    print(60+wc)
+else:
+    print('best case')
+if bc<=40:
+    print(60+bc)
+else:
+    print('worst case')
